1fb44037ae9ae5cedf9e257f8b02974247b9e463
Isaac Yeguas <isaac.yeguas@gmail.com>
2020-03-25 18:24:29 +0100

    3r comit integrate master

--- documents/Coronavirus.java
+++ documents/Coronavirus.java
@@ -1,5 +1,5 @@
 
-public class Coronavirus {
+public class Coronavirus{
 
     private float infeccio;
     private float mortaldat;
@@ -20,5 +20,43 @@ public class Coronavirus {
         infeccio = c.infeccio;
         mortaldat = c.mortaldat;  
     }
+
+    public float getInfeccio() {
+        return infeccio;
+    }
+
+    public void setInfeccio(float infeccio) {
+        this.infeccio = infeccio;
+    }
+
+    public float getMortaldat() {
+        return mortaldat;
+    }
+
+    public void setMortaldat(float mortaldat) {
+        this.mortaldat = mortaldat;
+    }   
     
+}
+
+class Covid_19 extends Coronavirus{
+    
+    private int diesDeQuarentena;
+
+    //constructor buit
+    Covid_19(){
+        super();
+        diesDeQuarentena = 0;
+    }
+     
+    Covid_19(int x) {
+        super();
+        diesDeQuarentena = x;
+    }
+    
+    //costructor copiador
+    Covid_19(Covid_19 cv) {
+        super();
+        diesDeQuarentena = cv.diesDeQuarentena;
+    }
 }
